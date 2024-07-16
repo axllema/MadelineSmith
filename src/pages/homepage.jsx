266a3f8d943e4f4776b4713dec91b702e7da73c1
axllema <axellexm@gmail.com>
2024-07-16 23:56:32 +0200

    changes problemes + changes background color nd all

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 import Loader from '../components/loader';
+import Header from '../layout/header.jsx';
 import Banner from '../components/banner';
 import Introduction from '../components/introduction';
 import BottomSection from '../components/bottomsection';
 import Playlist from '../components/playlist';
-import Footer from '../layouts/footer';
+import Footer from '../layout/footer';
 import '../scss/style.scss';
 import '../scss/pages/_homepage.scss';
-import Header from '../layout/header.jsx';
 
 function Homepage() {
     const [loading, setLoading] = useState(true);

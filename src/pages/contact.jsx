e9b6df4003241754cabd622780143e278f7fc876
axllema <axellexm@gmail.com>
2024-09-03 19:59:23 +0200

    new pics in contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,6 +5,10 @@ import Smiley1 from '../assets/img/smiley1.png';
 import Smiley2 from '../assets/img/smiley2.jpg';
 import Flower1 from '../assets/img/flower1.jpg';
 import Flower2 from '../assets/img/flower2.jpg';
+import Pearls1 from '../assets/img/pearls1.jpg';
+import LosSantos1 from '../assets/img/lossantos1.jpg';
+import MirrorSelfie from '../assets/img/mirror.jpg';
+import Maddss from '../assets/img/maddss.jpg';
 import Sun1 from '../assets/img/sun1.png';
 import Sun2 from '../assets/img/sun2.png';
 import Footer from '../layout/footer.jsx';
@@ -34,9 +38,6 @@ function Contact() {
 
             <div className="contact-container-1">
 
-                <img alt="Smiley" src={Smiley2} className='contact-container-smiley2' />
-                <img alt="Flower" src={Flower1} className='contact-container-smiley1 flower1' />
-
                 <div className="contact-infos-1">
 
                     <div className="background-info">
@@ -53,6 +54,25 @@ function Contact() {
                         </div>
                     </div>
 
+                    <div className="contact-images">
+                        <img alt="Smiley" src={Smiley2} className='contact-container-smiley2' />
+
+                        <img alt="Pearls" src={Pearls1} className='contact-container-pearls' />
+                        <img alt="Los Santos (Los Angeles)" src={LosSantos1} className='contact-container-lossantos' />
+
+                        <img alt="Flower" src={Flower1} className='contact-container-smiley1 flower1' />
+
+
+                        <img alt="Smiley" src={Smiley1} className='contact-container-smiley1' />
+
+                        <img alt="Maddy mirror selfie" src={MirrorSelfie} className='contact-container-mirrorselfie' />
+                        <img alt="Maddy coiffeuse" src={Maddss} className='contact-container-maddss' />
+
+                        <img alt="Smiley" src={Smiley2} className='contact-container-smiley2' />
+
+                        <img alt="Smiley" src={Flower2} className='contact-container-smiley2 flower2' />
+                    </div>
+
 
                 </div>
             </div>
@@ -64,9 +84,6 @@ function Contact() {
             </div>
             
             <div className="contact-container-2">
-
-                <img alt="Smiley" src={Smiley1} className='contact-container-smiley1' />
-                <img alt="Smiley" src={Flower2} className='contact-container-smiley2 flower2' />
 
                 <div className="contact-infos-2">
 
@@ -94,5 +111,3 @@ function Contact() {
 }
 
 export default Contact;
-
-// photo du pearls + photo ville + ...?
